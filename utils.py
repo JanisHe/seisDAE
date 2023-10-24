96d3eec3b2f125166b87970a33be94962eff9a36
Janis Heuel <janis.heuel@ruhr-uni-bochum.de>
2023-10-24 13:53:36 +0200

    Added function to check noise and singal files.

--- utils.py
+++ utils.py
@@ -150,18 +150,43 @@ def shift_array(array: np.array):
     return array
 
 
-def check_npz(npz_filename: str, verbose=False):
+def remove_file(filename: str, verbose=False):
+    """
+    Function deletes filenames.
+    """
+    if verbose is True:
+        print("File with error:", filename)
+    os.remove(filename)
+
+
+def check_npz(npz_filename: str, zero_check=False, verbose=False):
     """
     Trys to read npz file. If not the file is deleted
     :param npz_filename: filename to check
+    :param zero_check: If True, files that contains almost zeros will be deleted.
+    :param verbose: Default False. If True, files that are deleted will be printed.
     """
     try:
         dataset = np.load(npz_filename)
         data = dataset["data"]
+        if zero_check is True:
+            if np.max(np.abs(data)) <= 1e-15:
+                remove_file(filename=npz_filename, verbose=verbose)
     except Exception:
-        if verbose is True:
-            print("File with error:", npz_filename)
-        os.remove(npz_filename)
+        remove_file(filename=npz_filename, verbose=verbose)
+
+
+def check_signal_zeros(signal_dir: str, extension="npz", verbose=False):
+    """
+    Function checks if signal files contain a file with zeros, since these files are not allowed for training.
+    :param signal_dir: Full pathname for signal files
+    :param extension: Extension of filenames, default is npz.
+    :param verbose: Default False. If True, files that are deleted will be printed.
+    """
+    files = glob.glob(os.path.join(signal_dir, f"*.{extension}"))
+    # Loop over all files and check whether the data can be read and the file does not contain zeros only.
+    for filename in files:
+        check_npz(npz_filename=filename, zero_check=True, verbose=verbose)
 
 
 def check_noise_files(noise_dirname: str, extension="npz", **kwargs):
@@ -177,15 +202,68 @@ def check_noise_files(noise_dirname: str, extension="npz", **kwargs):
         check_npz(npz_filename=filename, **kwargs)
 
 
+def old_data_augmentation(signal_npz_file, noise_npz_file, ts_length=6001):
+    signal = np.load(signal_npz_file)
+    noise = np.load(noise_npz_file)
+    # Read signal and noise from npz files
+    try:
+        p_samp = signal["itp"]  # Sample of P-arrival
+        s_samp = signal["its"]  # Sample of S-arrival
+    except KeyError:
+        p_samp = None
+        s_samp = None
+
+    # Read data arrays from signal and noise
+    signal = signal["data"]
+    noise = noise["data"][:ts_length]
+
+    # epsilon = 0  # Avoiding zeros in added arrays
+    # shift1 = np.random.uniform(low=-1, high=1, size=int(self.ts_length - s_samp)) * epsilon
+    # TODO: Check data augmentation if correct; Ignore itp and its
+    # signal = shift_array(array=signal)
+    # signal = signal[:self.ts_length]
+    if p_samp and s_samp:
+        if int(ts_length - s_samp) < 0:
+            shift1 = np.zeros(0)
+        else:
+            shift1 = np.zeros(shape=int(ts_length - s_samp))
+        signal = np.concatenate((shift1, signal))
+        # Cut signal to length of ts_length and arrival of P-phase is included
+        p_samp += len(shift1)
+        s_samp += len(shift1)
+        start = random.randint(0, p_samp)
+        signal = signal[start:start + ts_length]
+    else:  # XXX Add case just for p_samp
+        if ts_length > len(signal):
+            start = random.randint(0, len(signal) - ts_length - 1)
+            signal = signal[start:int(start + ts_length)]
+        else:
+            signal = signal[:ts_length]
+
+    return signal, noise
+
+
 if __name__ == "__main__":
     import glob
     import matplotlib.pyplot as plt
     import random
 
-    files = glob.glob("/home/janis/CODE/seismic_denoiser/example_data/signal/*")
-    d = np.load(random.choice(files))
+    # files = glob.glob("/home/janis/CODE/seismic_denoiser/example_data/signal/*")
+    # d = np.load(random.choice(files))
+    # data = d["data"]
+    # shifted = shift_array(data)
+    # plt.plot(shifted[:6001])
+    # plt.show()
+
+    files_signal = glob.glob("/home/janis/CODE/seismic_denoiser/example_data/signal/*")
+    files_noise = glob.glob("/home/janis/CODE/seismic_denoiser/example_data/noise/*")
+    signal = random.choice(files_signal)
+    print(signal)
+    s, n = old_data_augmentation(signal_npz_file=signal,
+                                 noise_npz_file=random.choice(files_noise))
+    d = np.load(signal)
     data = d["data"]
     shifted = shift_array(data)
-    plt.plot(shifted[:6001])
+    plt.plot(s, alpha=0.5)
+    plt.plot(shifted[:6001], alpha=0.5)
     plt.show()
-
